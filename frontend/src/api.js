89b21408aec8c7e848356d422a85d4672a86a95b
mamunsail <aknet.mamun@gmail.com>
2026-04-28 00:48:45 +0600

    update api.js config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.68.50:8000",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 // Attach token to every request
